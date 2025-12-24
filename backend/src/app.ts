4c9c83d7ce1833fa58a8114cdecfe1cab73ba967
lautarosard <lautarosardina@hotmail.com>
2025-12-24 20:49:10 -0300

    fix of app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,21 +1,21 @@
-import express, {type Application } from 'express';
+import express, { type Application } from 'express';
 import cors from 'cors';
-
+import { errorHandler } from './api/middlewares/errorHandler.js';
 // Ya no importamos 'urlRoutes' directamente aquí.
 // Recibimos el router como dependencia.
 export const createApp = (urlRouter: express.Router): Application => {
-    const app: Application = express();
+  const app: Application = express();
 
-    app.use(cors());
-    app.use(express.json());
+  app.use(cors());
+  app.use(express.json());
 
-    // Montamos las rutas que nos pasaron por parámetro
-    app.use('/api/url', urlRouter);
+  // Montamos las rutas que nos pasaron por parámetro
+  app.use('/api/url', urlRouter);
 
-    app.get('/health', (req, res) => {
-        res.json({ status: 'OK', message: 'Shortener API is running' });
-    });
-    app.use(errorHandler);
-    
-    return app;
+  app.get('/health', (req, res) => {
+    res.json({ status: 'OK', message: 'Shortener API is running' });
+  });
+  app.use(errorHandler);
+
+  return app;
 };
